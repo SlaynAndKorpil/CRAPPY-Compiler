1202d7ae5a5207d40196f40614bd4600d659b2dd
FelixL <50499590+SlaynAndKorpil@users.noreply.github.com>
2019-10-03 16:52:21 +0200

    Fixed error in parse_compile_flags

--- CRAPPY-Compiler/compilerflags.cpp
+++ CRAPPY-Compiler/compilerflags.cpp
@@ -11,10 +11,10 @@ bool is_flag_set(unsigned int flag) {
 
 int parse_compiler_flag(char flag) {
     switch (flag) {
-    case 'I' | 'i': return DEBUGGER_INFORMATION_FLAG;
-    case 'H' | 'h': return HELP_FLAG;
-    case 'D' | 'd': return BREAK_ON_UNUSED_DEFINE_FLAG;
-    case 'M' | 'm': return BREAK_ON_UNUSED_MARKER_FLAG;
+    case 'i': return DEBUGGER_INFORMATION_FLAG;
+    case 'h': return HELP_FLAG;
+    case 'd': return BREAK_ON_UNUSED_DEFINE_FLAG;
+    case 'm': return BREAK_ON_UNUSED_MARKER_FLAG;
     default: return 0;
     }
 }
